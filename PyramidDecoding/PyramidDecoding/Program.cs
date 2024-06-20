cdf684765dac23fd6a01f1909f1a373d6eef1f50
suflors <iammagical.space@gmail.com>
2024-06-19 21:04:52 -0700

    read the input file and parsed it into a dictionary, wrote temporary code outputting it to the console to ensure it is sorted

--- PyramidDecoding/PyramidDecoding/Program.cs
+++ PyramidDecoding/PyramidDecoding/Program.cs
@@ -4,7 +4,20 @@
 	{
 		static void Main()
 		{
-			Console.WriteLine("Hello, World!");
+			string filepath = "coding_qual_input.txt";
+			string input = File.ReadAllText(filepath);
+			string[] lines = input.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+			Dictionary<int, string> words = [];
+			foreach (string line in lines)
+			{
+				string[] parts = line.Split(' ');
+				words.Add(int.Parse(parts[0]), parts[1]);
+			}
+			for (int i = 0; i < words.Count; i++)
+			{
+				var word = words[i + 1];
+				Console.WriteLine($"{i + 1} {word}");
+			}
 		}
 	}
 }
